23edf3a3430610a07525b9adf69e76b3bd1e4429
PierreB <pierre@cabnum.fr>
2021-03-08 13:51:26 +0100

    add viewBinding

--- app/src/main/java/eu/pbenayoun/wikireferences2/MainActivity.kt
+++ app/src/main/java/eu/pbenayoun/wikireferences2/MainActivity.kt
@@ -2,27 +2,25 @@ package eu.pbenayoun.wikireferences2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import dagger.hilt.android.AndroidEntryPoint
-import eu.pbenayoun.repository.referencesrepository.ReferencesCallback
-import eu.pbenayoun.repository.referencesrepository.ReferencesRepository
-import javax.inject.Inject
+import eu.pbenayoun.wikireferences2.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     lateinit var referencesRepositoryViewModel: ReferencesRepositoryViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         referencesRepositoryViewModel = ViewModelProvider(this).get(ReferencesRepositoryViewModel::class.java)
     }
 
     override fun onResume() {
         super.onResume()
-        referencesRepositoryViewModel.getReferences("test")
+
     }
 
 
