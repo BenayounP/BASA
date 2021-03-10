e496933509195c36440a8d0fd6cd5f0b8a0cd440
PierreB <pierre@cabnum.fr>
2021-03-10 13:31:00 +0100

    update mockedrepository to throw error sometimes^^

--- repository/src/main/java/eu/pbenayoun/repository/referencesrepository/MockedReferencesRepository.kt
+++ repository/src/main/java/eu/pbenayoun/repository/referencesrepository/MockedReferencesRepository.kt
@@ -5,16 +5,36 @@ import javax.inject.Inject
 
 class MockedReferencesRepository @Inject constructor(): ReferencesRepository {
     val requestDelay = 500L
+    var launchError = false
+
     override fun getReferences(query : String, referencesCallBackHandler: (callback:ReferencesCallback) -> Unit) {
         GlobalScope.launch(Dispatchers.Default) {
             launch(Dispatchers.Main) {
                 referencesCallBackHandler(ReferencesCallback.fetching(query))
             }
             delay(requestDelay)
-            val references = (1..1000).shuffled().first()
-            launch(Dispatchers.Main) {
-                referencesCallBackHandler(ReferencesCallback.Success(ReferencesSuccessModel(query, references)))
+            when (launchError){
+                true -> launchError(this,query,referencesCallBackHandler)
+                false -> launchSuccess(this,query,referencesCallBackHandler)
             }
+            launchError=!launchError
+        }
+    }
+
+    fun launchSuccess(coroutineScope: CoroutineScope,
+                    query : String,
+                    referencesCallBackHandler: (callback:ReferencesCallback) -> Unit) : Job{
+        val references = (1..1000).shuffled().first()
+        return coroutineScope.launch(Dispatchers.Main) {
+            referencesCallBackHandler(ReferencesCallback.Success(ReferencesSuccessModel(query, references)))
+        }
+    }
+
+    fun launchError(coroutineScope: CoroutineScope,
+                    query : String,
+                    referencesCallBackHandler: (callback:ReferencesCallback) -> Unit) : Job{
+        return  coroutineScope.launch(Dispatchers.Main) {
+            referencesCallBackHandler(ReferencesCallback.Error(query,ReferencesErrorType.NoNetwork()))
         }
     }
 
