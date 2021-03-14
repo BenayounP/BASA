8221395d73a310e65c7cbde83ecd6e66611383e5
PierreB <pierre@cabnum.com>
2021-03-14 17:15:31 +0100

    fix ui colors and margin

--- repository/src/main/java/eu/pbenayoun/repository/referencesrepository/MockedReferencesRepository.kt
+++ repository/src/main/java/eu/pbenayoun/repository/referencesrepository/MockedReferencesRepository.kt
@@ -1,5 +1,6 @@
 package eu.pbenayoun.repository.referencesrepository
 
+import android.util.Log
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
@@ -7,12 +8,19 @@ class MockedReferencesRepository @Inject constructor(): ReferencesRepository {
     val requestDelay = 500L
     var launchError = false
 
+    init{
+        Log.d("TMP_DEBUG","MockedReferencesRepository init")
+    }
+
     override fun getReferences(query : String, referencesCallBackHandler: (callback:ReferencesCallback) -> Unit) {
         GlobalScope.launch(Dispatchers.Default) {
+            Log.d("TMP_DEBUG","get references launch")
             launch(Dispatchers.Main) {
+                Log.d("TMP_DEBUG","get references announce fetching")
                 referencesCallBackHandler(ReferencesCallback.fetching(query))
             }
             delay(requestDelay)
+            Log.d("TMP_DEBUG","get references after delay")
             when (launchError){
                 true -> launchError(this,query,referencesCallBackHandler)
                 false -> launchSuccess(this,query,referencesCallBackHandler)
